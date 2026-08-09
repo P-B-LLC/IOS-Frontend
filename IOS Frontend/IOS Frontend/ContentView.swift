0d790c2ee17ad8a150904c0c4516548d934f8e56
Aaron Pio <aaronpio18@gmial.com>
2026-08-09 15:34:29 -0500

    Add initial Xcode project (IOS Frontend app)

---
+++ IOS Frontend/IOS Frontend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS Frontend
+//
+//  Created by user299988 on 8/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS Frontend/IOS Frontend/IOS_FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_FrontendApp.swift
+//  IOS Frontend
+//
+//  Created by user299988 on 8/9/26.
+//
+
+import SwiftUI
+
+@main
+struct IOS_FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
