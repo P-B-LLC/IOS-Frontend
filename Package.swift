4df5731b2edba3fbd2d8f18c0be76ebecdb7a754
AaronJohnPio <apio@uic.edu>
2026-08-10 21:12:38 -0400

    Support API generation on macOS

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "RepbaseAPI",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v17),
+        .macOS(.v10_15)
     ],
     products: [
         .library(name: "RepbaseAPI", targets: ["RepbaseAPI"])
